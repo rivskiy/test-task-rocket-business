0ceddb12da62de5c16a3db4d6be3bc59ada0f051
Aleksey Pechurin <pe4urin@yandex.ru>
2023-06-03 16:25:58 +0300

    добавлен адаптив и логика кнопки меню

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
-$(function () {
 
-})
+const menu = document.querySelector('.menu');
+const menuBtn = document.querySelector('.menu-btn');
+const menuLinks = document.querySelectorAll('.menu__list-item');
+
+const showMenu = () => {
+  menu.classList.toggle('menu--active');
+  menuBtn.classList.toggle('menu-btn--active');
+  // document.body.classList.toggle('body--block');
+};
+
+menuBtn.addEventListener('click', showMenu);
+
+for (let link of menuLinks) {
+  link.addEventListener('click', showMenu)
+};
